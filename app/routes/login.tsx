59e20030637188de8e6bd364124ef394f180f6e6
Trevor Davis <trevor.davis@dutchie.com>
2023-09-15 15:53:32 -0400

    Revert "log it to confirm"
    This reverts commit 6aadc0a0fa0d337d51fc96cfcc5cc73c2a5881f2.

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -22,8 +22,6 @@ export default function Login() {
         flow: "auth-code",
     });
 
-    console.log({ redirectUri: ENV.GOOGLE_REDIRECT_URI });
-
     return (
         <div className="mx-auto mt-12 flex justify-center">
             <button
