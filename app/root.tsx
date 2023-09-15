f6117eddc1c31252e3af07403fbb912a3f79562c
Trevor Davis <trevor.davis@dutchie.com>
2023-09-15 13:06:15 -0400

    first pass at sign in

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import { json, type LinksFunction, type LoaderFunction } from "@remix-run/node";
 import {
     Link,
     Links,
@@ -7,50 +7,66 @@ import {
     Outlet,
     Scripts,
     ScrollRestoration,
+    useLoaderData,
     useNavigation,
 } from "@remix-run/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import styles from "./tailwind.css";
 import { Loader } from "./components/loader";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
+export const loader: LoaderFunction = async () => json({
+    ENV: {
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    },
+});
+
 export default function App() {
     const navigation = useNavigation();
+    const { ENV } = useLoaderData();
 
     return (
-        <html lang="en">
-            <head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width,initial-scale=1"
-                />
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                <header className="bg-gray-700 text-white p-3 mb-6">
-                    <div className="max-w-5xl mx-auto flex justify-between items-center">
-                        <Link to="/">North Cannabis Co</Link>
-                        <div>
-                            <Link to="/cart">Cart</Link>
+        <GoogleOAuthProvider clientId={ENV.GOOGLE_CLIENT_ID}>
+            <html lang="en">
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width,initial-scale=1"
+                    />
+                    <Meta />
+                    <Links />
+                </head>
+                <body>
+                    <header className="bg-gray-700 text-white p-3 mb-6">
+                        <div className="max-w-5xl mx-auto flex justify-between items-center">
+                            <Link to="/">North Cannabis Co</Link>
+                            <ul className="flex">
+                                <li className="border-r border-current mr-3 pr-3">
+                                    <Link to="/login" className="block">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/cart" className="block">Cart</Link>
+                                </li>
+                            </ul>
                         </div>
-                    </div>
-                </header>
+                    </header>
 
-                <Outlet />
+                    <Outlet />
 
-                <footer className="bg-gray-700 text-white p-3 mt-12 text-center">
-                    © {new Date().getFullYear()}
-                </footer>
+                    <footer className="bg-gray-700 text-white p-3 mt-12 text-center">
+                        © {new Date().getFullYear()}
+                    </footer>
 
-                <ScrollRestoration />
-                <Scripts />
-                <LiveReload />
+                    <ScrollRestoration />
+                    <Scripts />
+                    <LiveReload />
 
-                {navigation.state === "loading" ? <Loader /> : null}
-            </body>
-        </html>
+                    {navigation.state === "loading" ? <Loader /> : null}
+                </body>
+            </html>
+        </GoogleOAuthProvider>
     );
 }
---
+++ app/routes/login.tsx
@@ -0,0 +1,36 @@
+import { type V2_MetaFunction, type LoaderFunction, json } from "@remix-run/node";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useLoaderData } from "@remix-run/react";
+
+export const meta: V2_MetaFunction = () => {
+    return [{ title: "Login" }];
+};
+
+export const loader: LoaderFunction = async () =>
+    json({
+        ENV: {
+            GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
+        },
+    });
+
+export default function Login() {
+    const { ENV } = useLoaderData();
+
+    const login = useGoogleLogin({
+        redirect_uri: ENV.GOOGLE_REDIRECT_URI,
+        ux_mode: "redirect",
+        flow: "auth-code",
+    });
+
+    return (
+        <div className="mx-auto mt-12 flex justify-center">
+            <button
+                onClick={() => login()}
+                className="bg-green-400 p-2 rounded-md"
+            >
+                Sign in with Google
+            </button>
+        </div>
+    );
+}
+
