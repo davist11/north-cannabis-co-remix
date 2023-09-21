f68824187acfe814de5d7ac6f3981940f10193f9
Trevor Davis <trevor.davis@dutchie.com>
2023-09-21 11:22:16 -0400

    remove old button and packages

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,5 @@
 import { type V2_MetaFunction, type LoaderFunction, json } from "@remix-run/node";
-import { useGoogleLogin } from "@react-oauth/google";
 import { useLoaderData } from "@remix-run/react";
-import { v4 as uuidv4 } from "uuid";
 
 export const meta: V2_MetaFunction = () => {
     return [{ title: "Login" }];
@@ -10,7 +8,6 @@ export const meta: V2_MetaFunction = () => {
 export const loader: LoaderFunction = async () =>
     json({
         ENV: {
-            GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
             AUTH_URI: process.env.AUTH_URI,
         },
     });
@@ -18,31 +15,15 @@ export const loader: LoaderFunction = async () =>
 export default function Login() {
     const { ENV } = useLoaderData();
 
-    const login = useGoogleLogin({
-        redirect_uri: ENV.GOOGLE_REDIRECT_URI,
-        ux_mode: "redirect",
-        flow: "auth-code",
-        state: uuidv4(),
-    });
-
     return (
         <div className="mx-auto mt-12">
-            <div className="flex justify-center mb-6">
-                <button
-                    onClick={() => login()}
-                    className="bg-green-400 p-2 rounded-md"
-                >
-                    Sign in with Google
-                </button>
-            </div>
             <div className="flex justify-center">
                 <form method="post" action={ENV.AUTH_URI}>
-                    <input type="hidden" name="_csrf" value={uuidv4()} />
                     <button
                         type="submit"
-                        className="bg-blue-400 p-2 rounded-md"
+                        className="bg-green-400 p-2 rounded-md"
                     >
-                        Sign in
+                        Login with Google
                     </button>
                 </form>
             </div>
